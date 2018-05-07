982ef84aa8baa22aca296c166a6eb0d8fb114198
Tiamo <Tiamo>
2018-05-07 16:45:32 +0800

    数据分析:封装常量

---
+++ src/main/scala/com/kugou/constants/GlobalContants.scala
@@ -0,0 +1,17 @@
+package com.kugou.constants
+
+/**
+  * Created by idea on 2018/5/7.
+  */
+object GlobalContants {
+
+  //默认值
+  final val DEFAULT_VALUE = "unknown"
+  //任务运行日期
+  final val TASK_RUN_DATE = "task_run_date"
+  //hdfs上日志的存放目录
+  final val HDFS_LOG_PATH_PREFIX: String= "/kuting_logs"
+  //任务的完整输入路径
+  final val TASK_INPUT_PATH: String = "task_input_path"
+
+}
